1c4e733bf3f9e2291f1800db019955122ec10ca5
RobinDBHW <thyeria@gmx.de>
2023-05-06 00:04:02 +0200

    finished a1

--- uebung0/a1.py
+++ uebung0/a1.py
@@ -1,6 +1,8 @@
 import pytest
 
+
 def naive_div(a, b):
+    """Returns the quotient of a/b such as the remainder of the division as (quotient, remainder)"""
     if b == 0:
         raise ZeroDivisionError('division by zero is not defined')
     elif b < 0:
@@ -9,7 +11,8 @@ def naive_div(a, b):
     while a >= 0:
         a -= b
         r += 1
-    return r, b+a
+    return r, b + a
+
 
 @pytest.mark.parametrize('a, b, expected',
                          [(1, 1, (1, 0)), (3, 2, (1, 1))])
@@ -19,3 +22,30 @@ def test_nativ_dif(a, b, expected):
         naive_div(1, 0)
     with pytest.raises(NotImplementedError):
         naive_div(1, -1)
+
+
+def div(a, b):
+    """Returns the quotient of a/b such as the remainder of the division as (quotient, remainder)"""
+    if b == 0:
+        raise ZeroDivisionError('division by zero is not defined')
+    elif b < 0:
+        raise NotImplementedError('division by negative numbers is not implemented')
+    n = a.bit_length()
+    tmp = b << n
+    r = 0
+    for _ in range(0, n + 1):
+        r <<= 1
+        if tmp <= a:
+            a -= tmp
+            r += 1
+        tmp >>= 1
+    return r, a
+
+@pytest.mark.parametrize('a, b, expected',
+                         [(1, 1, (1, 0)), (3, 2, (1, 1))])
+def test_div(a, b, expected):
+    assert div(a,b) == expected
+    with pytest.raises(ZeroDivisionError):
+        div(1, 0)
+    with pytest.raises(NotImplementedError):
+        div(1, -1)
