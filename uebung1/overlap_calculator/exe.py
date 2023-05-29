609eef6e9c2d8d4bb42c01cdab15d55f4d5ac21d
RobinDBHW <thyeria@gmx.de>
2023-05-30 00:14:42 +0200

    added a test

--- uebung1/overlap_calculator/exe.py
+++ uebung1/overlap_calculator/exe.py
@@ -1,21 +1,45 @@
-import os.path
-import molecule
-import matplotlib.pyplot as plt
+import atom as at
+import molecule as mol
 import basis_set as bs
+import matplotlib.pyplot as plt
 
+# Coordinates are in the unit of Angstrom.
+o1 = at.Atom('O', [0.000, 0.000, 0.000], unit='A')
+h1 = at.Atom('H', [1.000, 0.000, 0.000], unit='A')
+h2 = at.Atom('H', [0.000, 1.000, 0.000], unit='A')
 
-if not os.path.isfile('overlap.py'): exec(open('init.py').read())
+# Problem 1 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# a)--------------------------------------------------------------------------------------------------------------------
 
-molecule = molecule.from_xyz('Aniline.xyz', bs.VSTO3G)
+h2o = mol.Molecule([o1, h1, h2], bs.VSTO3G)
+h2o.produce_S()
 
-
-molecule.eht_hamiltonian()
-molecule.solve_eht()
-molecule.eht_total_energy()
-molecule.klopman_repulsion_energies()
-print(molecule.get_total_energy_klopman_eht())
-
-#molecule.produce_S()
-#plt.matshow(molecule.S)
+# print(h2o.S)
+#plt.matshow(h2o.S)
 #plt.show()
 
+# b)--------------------------------------------------------------------------------------------------------------------
+
+h2o.eht_hamiltonian()
+print(h2o.EHT_H)
+
+# c)--------------------------------------------------------------------------------------------------------------------
+
+h2o.solve_eht()
+print(h2o.EHT_MO_Energies)
+print(h2o.EHT_MOs)
+
+# d)--------------------------------------------------------------------------------------------------------------------
+
+h2o.eht_total_energy()
+print(h2o.EHT_Total_Energy)
+
+# Problem 2 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+# a), b)----------------------------------------------------------------------------------------------------------------
+
+h2o.klopman_repulsion_energies()
+print(h2o.KLOPMAN_ELEC_REP_Energy)
+print(h2o.KLOPMAN_NUC_REP_Energy)
+
+# c)--------------------------------------------------------------------------------------------------------------------
+print(h2o.get_total_energy_klopman_eht())
