8399a50c3d85e6d9178ba693a333d6ae3b56b022
Johannes Greiner <johannes.greiner@tum.de>
2023-06-17 08:24:04 +0200

    add plottung

--- uebung1/plotMOs.py
+++ uebung1/plotMOs.py
@@ -3,16 +3,16 @@ import basis_set as bs
 import numpy as np
 from mayavi import mlab
 
-molecule = mol.from_xyz('test_data/xyz_files/1H-Pyrazole.xyz',basis_set=bs.VSTO3G)
+molecule = mol.from_xyz('test_data/xyz_files/Benzene.xyz',basis_set=bs.VSTO3G)
 molecule.calc_S()
 molecule.eht_hamiltonian()
 molecule.solve_eht()
 
-orbital = molecule.n_velectrons//2 #homo
+orbital = molecule.n_velectrons//2-1 #homo
 
-x_ = np.linspace(-3., 3., 60)
-y_ = np.linspace(-3., 3., 60)
-z_ = np.linspace(-2., 2., 40)
+x_ = np.linspace(-5., 5., 100)
+y_ = np.linspace(-5., 5., 100)
+z_ = np.linspace(-5., 5., 100)
 x, y, z = np.meshgrid(x_, y_, z_, indexing='ij')
 def V(x,y,z):
     coords = np.stack((np.ndarray.flatten(x),np.ndarray.flatten(y),np.ndarray.flatten(z)), axis=-1)
@@ -21,6 +21,7 @@ def V(x,y,z):
         gValues[i,:] = molecule.EHT_MOs[i,orbital]*gauss.evaluate(coords)
 
     return np.sum(gValues,axis=0).reshape((np.size(x_),np.size(y_),np.size(z_)))
-print(V(x,y,z).shape)
 
-mlab.contour3d(x, y, z, V)
+mlab.contour3d(x, y, z, V, color=(1,0,0), contours=[0.05])
+mlab.contour3d(x, y, z, V, color=(0,0,1), contours=[-0.05])
+mlab.show()
